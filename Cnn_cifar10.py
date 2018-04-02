4e2752fb02b902f90f4fdf281e7a7f3a758538c7
Anshuk Uppal <Anshuk.Uppal@iiitb.org>
2018-04-02 08:53:37 +0530

    Update Cnn_cifar10.py
    _________________________________________________________________
    Layer (type)                 Output Shape              Param #   
    =================================================================
    conv2d_1 (Conv2D)            (None, 20, 32, 32)        540       
    _________________________________________________________________
    dropout_1 (Dropout)          (None, 20, 32, 32)        0         
    _________________________________________________________________
    conv2d_2 (Conv2D)            (None, 40, 26, 26)        39240     
    _________________________________________________________________
    dropout_2 (Dropout)          (None, 40, 26, 26)        0         
    _________________________________________________________________
    conv2d_3 (Conv2D)            (None, 40, 20, 20)        78440     
    _________________________________________________________________
    max_pooling2d_1 (MaxPooling2 (None, 40, 10, 10)        0         
    _________________________________________________________________
    flatten_1 (Flatten)          (None, 4000)              0         
    _________________________________________________________________
    dense_1 (Dense)              (None, 128)               512128    
    _________________________________________________________________
    dropout_3 (Dropout)          (None, 128)               0         
    _________________________________________________________________
    dense_2 (Dense)              (None, 10)                1290      
    =================================================================
    Total params: 631,638
    Trainable params: 631,638
    Non-trainable params: 0
    _________________________________________________________________

--- Cnn_cifar10.py
+++ Cnn_cifar10.py
@@ -2,7 +2,7 @@ import numpy
 from keras.datasets import cifar10
 from keras.models import Sequential
 from keras.layers import Dense,Dropout,Flatten
-from keras.layers.convolutional import Conv3D,MaxPooling3D
+from keras.layers.convolutional import Conv2D,MaxPooling2D
 from keras.optimizers import SGD
 from keras.utils import np_utils
 from keras import backend as K
@@ -19,12 +19,12 @@ y_test = np_utils.to_categorical(y_test)
 num_classes = y_test.shape[1]
 
 model3=Sequential()
-model3.add(Conv3D(20,(3,3,3),strides=(1,1,1),input_shape=(500,3,32,32),padding="same",data_format="channels_first",use_bias=False))
+model3.add(Conv2D(20,(3,3),strides=(1,1),input_shape=(3,32,32),padding="same",data_format="channels_first",use_bias=False))
 model3.add(Dropout(0.2))
-model3.add(Conv3D(40,(3,7,7),strides=(1,1,1),padding="valid",activation='relu',use_bias=True))
+model3.add(Conv2D(40,(7,7),strides=(1,1),padding="valid",activation='relu',use_bias=True))
 model3.add(Dropout(0.4))
-model3.add(Conv3D(40,(1,7,7),strides=(1,1,1),padding="valid",activation='relu',use_bias=True))
-model3.add(MaxPooling3D(pool_size=(1,2,2),padding='valid'))
+model3.add(Conv2D(40,(7,7),strides=(1,1),padding="valid",activation='relu',use_bias=True))
+model3.add(MaxPooling2D(pool_size=(2,2),padding='valid'))
 model3.add(Flatten())
 model3.add(Dense(128, activation='relu'))
 model3.add(Dropout(0.5))
@@ -36,4 +36,13 @@ decay = lrate/epochs
 sgd = SGD(lr=lrate, momentum=0.9, decay=decay, nesterov=False)
 model3.compile(loss='categorical_crossentropy', optimizer=sgd, metrics=['accuracy'])
 print(model3.summary())
+
+
+# Fit the model
+model3.fit(X_train[:40000], Y_train[:40000], validation_data=(x_test, y_test), epochs=epochs, batch_size=100)
+# Final evaluation of the model
+scores = model3.evaluate(X_train, y_test, verbose=0)
+print("Accuracy: %.2f%%" % (scores[1]*100))
+
+########################
 ########################
